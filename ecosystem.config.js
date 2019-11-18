7bb5e6e0cc0a7f9fcb4c5452813eb6676465447c
Joshua Jung <josh@jungdigital.com>
2019-11-18 21:00:26 +0100

    Update NODE_ENV

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
     script: "./server/src/index.js",
     env: {
       HTTP_PORT: 11002,
+      NODE_ENV: 'production',
       MONGO_DB_URI: 'mongodb://ec2-13-59-48-224.us-east-2.compute.amazonaws.com:10017'
     }
   }]
