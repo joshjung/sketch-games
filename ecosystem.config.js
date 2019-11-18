69b625b1383e642eab8f8b01c55eaf56c259075b
Joshua Jung <josh@jungdigital.com>
2019-11-18 21:28:02 +0100

    Adding key file and passphrase settings to PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,11 @@ module.exports = {
     script: "./server/src/index.js",
     env: {
       HTTP_PORT: 11002,
+      HTTPS_PORT: 11002,
       NODE_ENV: 'production',
-      MONGO_DB_URI: 'mongodb://ec2-13-59-48-224.us-east-2.compute.amazonaws.com:10017'
+      MONGO_DB_URI: 'mongodb://ec2-13-59-48-224.us-east-2.compute.amazonaws.com:10017',
+      SSL_KEY_FILE: '/home/ubuntu/certs/gamepen.joshjung.com.key',
+      SSL_CERT_FILE: '/home/ubuntu/certs/gamepen.joshjung.com.cert_chain.crt'
     }
   }]
-}
+};
